2ad4f5e8ee47dea5cb69e485fa08a885c03109d1
erwanlr <erwan.lr@gmail.com>
2013-08-30 15:43:40 +0200

    Version output fix

--- lib/common/controllers/common_controller.rb
+++ lib/common/controllers/common_controller.rb
@@ -5,6 +5,8 @@ require_relative('controller.rb') # Line required as the current file is loaded 
 class CommonController < Controller
 
   def initialize
+    @updater = UpdaterFactory.get_updater(ROOT_DIR)
+
     register_options(
       ['-v', '--verbose', 'Verbose output'],
       ['-V', '--version', 'Output the WPScan version'],
@@ -24,21 +26,20 @@ class CommonController < Controller
 
   # Ending tag must be in the template (because the script will exit after tehe action)
   def version
-    @version = REVISION ? "v#{WPSCAN_VERSION}r#{REVISION}" : "v#{WPSCAN_VERSION}"
+    revision = @updater.local_revision_number() if @updater
+    @version = revision ? "v#{WPSCAN_VERSION}r#{revision}" : "v#{WPSCAN_VERSION}"
   end
 
   # Ending tag must be in the template (because the script will exit after tehe action)
   def update
-    updater = UpdaterFactory.get_updater(ROOT_DIR)
-
-    if !updater.nil?
-      if updater.has_local_changes?
+    if !@updater.nil?
+      if @updater.has_local_changes?
         if user_interaction?
           print "#{red('[!]')} Local file changes detected, an update will override local changes, do you want to continue updating? [y/n] "
 
           if Readline.readline =~ /^y/i
-            updater.reset_head
-            @update_results = updater.update()
+            @updater.reset_head
+            @update_results = @updater.update()
           else
             @aborted = true
           end
@@ -46,7 +47,7 @@ class CommonController < Controller
           @local_changes = true
         end
       else
-        @update_results = updater.update()
+        @update_results = @updater.update()
       end
     else
       @not_installed = true
